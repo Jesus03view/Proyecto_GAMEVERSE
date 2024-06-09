7fbd0c6252388a816546d28655d435b107090854
Jesus03view <jesusemelyjuanchoproyecto@gmail.com>
2024-06-09 13:06:20 -0500

    Actualización del controlador e iplementación de estilos en la vista registro

--- src/Controllers/Controller_View_Sign_in.java
+++ src/Controllers/Controller_View_Sign_in.java
@@ -2,9 +2,33 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller_View_Sign_in implements Initializable {
+
+    @FXML
+    private TextField txt_Nombre;
+    @FXML
+    private TextField txt_Identificacion;
+    @FXML
+    private TextField txt_Celular;
+    @FXML
+    private TextField txt_Correo;
+    @FXML
+    private TextField txt_Nombre_jugador;
+    @FXML
+    private PasswordField txt_Password_1;
+    @FXML
+    private PasswordField txt_Password_2;
+    @FXML
+    private Button btn_cancelar;
+    @FXML
+    private Button btn_aceptar;
 
     /**
      * Initializes the controller class.
@@ -12,6 +36,16 @@ public class Controller_View_Sign_in implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void EventAction(ActionEvent event) {
+
+        if (event.getSource() == btn_cancelar) {
+        
+        } else if (event.getSource() == btn_aceptar) {
+        
+        }
+    }
+
 }
