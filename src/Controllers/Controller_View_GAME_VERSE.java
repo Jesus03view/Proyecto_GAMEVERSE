6882d89f73a80d5c713671e90631b23bda5af761
Jesus03view <jesusemelyjuanchoproyecto@gmail.com>
2024-06-22 22:44:24 -0500

    Implementacion de opciones de usuario(50%)

--- src/Controllers/Controller_View_GAME_VERSE.java
+++ src/Controllers/Controller_View_GAME_VERSE.java
@@ -5,18 +5,38 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Controller_View_GAME_VERSE implements Initializable {
 
     @FXML
     private Button btn_carrito;
+    @FXML
+    private Button btn_Friends;
+    @FXML
+    private Button btn_addF;
+    @FXML
+    private Button btn_Amigos;
+    @FXML
+    private Button btn_Perfil;
+    @FXML
+    private Pane panelPerfil;
+    @FXML
+    private Button btn_CClave;
+    @FXML
+    private Button btn_ModO;
+    @FXML
+    private Button btn_CerrarS;
+    @FXML
+    private Pane panelAmigos;
 
     /**
      * Initializes the controller class.
@@ -42,6 +62,43 @@ public class Controller_View_GAME_VERSE implements Initializable {
             miStage.close();
         } catch (IOException ex) {
             Logger.getLogger(Controller_View_GAME_VERSE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void ActionEvent(ActionEvent e) {
+
+        if (e.getSource().equals(btn_Perfil)) {
+            panelPerfil.setVisible(!panelPerfil.isVisible());
+            panelAmigos.setVisible(false);
+
+        } else if (e.getSource().equals(btn_Amigos)) {
+            panelAmigos.setVisible(!panelAmigos.isVisible());
+            panelPerfil.setVisible(false);
+        } else if (e.getSource().equals(btn_Friends)) {
+            btn_Friends.getStyleClass().add("btnAfter");
+            btn_addF.getStyleClass().removeAll("btnAfter");
+
+        } else if (e.getSource().equals(btn_addF)) {
+            btn_addF.getStyleClass().add("btnAfter");
+            btn_Friends.getStyleClass().removeAll("btnAfter");
+        } else if (e.getSource().equals(btn_CerrarS)) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_Login.fxml"));
+
+                Parent roott = loader.load();
+
+                Scene scene = new Scene(roott);
+                Stage stage = new Stage();
+
+                stage.setScene(scene);
+                stage.show();
+
+                Stage miStage = (Stage) this.btn_carrito.getScene().getWindow();
+                miStage.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Controller_View_GAME_VERSE.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 }
