5e84d5ed61ec9b27c7498e881be3482cf1830d79
Jesus03view <jesusemelyjuanchoproyecto@gmail.com>
2024-06-24 01:52:55 -0500

    Primera Actualización de la ListaDobleUsuario

--- src/Models/EstructurasDeDatos/ListaDobleUsuario.java
+++ src/Models/EstructurasDeDatos/ListaDobleUsuario.java
@@ -1,5 +1,35 @@
 package Models.EstructurasDeDatos;
 
+import Models.Nodos.Nodo_Usuario;
+
 public class ListaDobleUsuario {
-    
+
+    private Nodo_Usuario cab;
+    private int nUsuarios;
+
+    public ListaDobleUsuario() {
+
+        this.cab = null;
+        this.nUsuarios = 0;
+    }
+
+    public Nodo_Usuario getCab() {
+        return cab;
+    }
+
+    public void setCab(Nodo_Usuario cab) {
+        this.cab = cab;
+    }
+
+    public int getnUsuarios() {
+        return nUsuarios;
+    }
+
+    public void setnUsuarios(int nUsuarios) {
+        this.nUsuarios = nUsuarios;
+    }
+
+    public void vaciarLista() {
+        cab = null;
+    }
 }
